722c9a142d39c29fcade575f8451c7b9f1b74e55
cestradaTesselar <cestrada@tesselar.mx>
2017-11-27 09:46:23 -0600

    modifiaciones server

--- api/aEstrella.js
+++ api/aEstrella.js
@@ -60,34 +60,34 @@ const Algoritmo = Matriz => {
         for (let renglon = (i < 1 ? 1 : i) - 1; renglon < i + 2; renglon++) {
             for (let columna = (j < 1 ? 1 : j) - 1; columna < j + 2; columna++) {
                 if (columna == j && renglon == i || columna >= Matriz.length || renglon >= Matriz.length
-                    // Esto era para saber si era el nodo inicio o si era uno intransitable
-                    // || Matriz[renglon][columna].Valor == 1 || Matriz[renglon][columna].Valor == 0
                     || !!Matriz[renglon][columna].final
                 ) continue;
 
                 if (Matriz[renglon][columna].Valor.final) {
                     return Matriz[renglon][columna];
                 }
-                // if (bandera) {
-                //     Acumulador = Matriz[renglon][columna].Valor + 1;
-                //     bandera = false;
-                // }
-                //Matriz[renglon][columna].Visitado = false;
-                // if (!Chidos.find(x => x.i == renglon && x.j == columna)) {
-                //     Frontera.push(Matriz[renglon][columna]);
-                // }
-                // contador++;
+
             }
             if (CostoTotal(Matriz[renglon][columna]) < CostoTotal(Aux)) {
                 Aux = Matriz[renglon][columna]
             }
-
-            // Matriz[renglon][columna].Visitado = CostoTotal(Matriz[renglon][columna]) < CostoTotal(Frontera[0]) ? true : false
         }
-    }
-    
+        Ordenar(Frontera)
 
-    Frontera.sort((a, b) => {
+        if (Frontera.length > 0 && CostoTotal(Frontera[0]) > Aux) {
+            Frontera.shift(Aux)
+            Aux.Visitado = true
+        }
+
+        Expandidos.push(Frontera.unshift(0))
+        VerVecinos(Expandidos[Expandidos.length - 1].i, Expandidos[Expandidos.length - 1].j)
+    }
+    // Expandidos.push(Frontera.shift());
+}
+
+
+const Ordenar = lista => {
+    lista.sort((a, b) => {
         if (CostoTotal(a) > CostoTotal(b)) {
             return 1;
         }
@@ -97,95 +97,8 @@ const Algoritmo = Matriz => {
         // a must be equal to b
         return 0;
     });
-
-    if(Frontera.length > 0 && CostoTotal(Frontera[0]) > Aux ){
-        Frontera.shift(Aux)
-        Aux.Visitado = true
-    }
-
-    Expandidos.push(Frontera.shift());
-
-
 }
 
-let Frontera = new Array();
-let Expandidos = new Array();
-let Chidos = new Array();
 
-const verVecinos = (Matriz, i, j) => {
-    let contador = 0;
-    let Acumulador = 0;
-    let bandera = true;
+module.exports.verVecinos = Algoritmo
 
-    for (let renglon = (i < 1 ? 1 : i) - 1; renglon < i + 2; renglon++) {
-        for (let columna = (j < 1 ? 1 : j) - 1; columna < j + 2; columna++) {
-            if (columna == j && renglon == i || columna >= Matriz.length || renglon >= Matriz.length
-                || Matriz[renglon][columna].Valor == 1 || Matriz[renglon][columna].Valor == 0
-            ) continue;
-
-            if (Matriz[renglon][columna].Valor == -1) {
-                return Matriz[renglon][columna];
-            }
-            if (bandera) {
-                Acumulador = Matriz[renglon][columna].Valor + 1;
-                bandera = false;
-            }
-
-            //Matriz[renglon][columna].Visitado = false;
-            if (!Chidos.find(x => x.i == renglon && x.j == columna)) {
-                Frontera.push(Matriz[renglon][columna]);
-            }
-            contador++;
-        }
-        Frontera.sort((a, b) => {
-            if (a.gDeN > b.gDeN) {
-                return 1;
-            }
-            if (a.gDeN < b.gDeN) {
-                return -1;
-            }
-            // a must be equal to b
-            return 0;
-        });
-    }
-    Expandidos.push(Frontera.shift());
-
-    if (Expandidos.length > 0) {
-        let tempato = Expandidos.shift();
-        tempato.Visitado = true;
-        Chidos.push(tempato);
-        verVecinos(Matriz, tempato.i, tempato.j);
-    }
-
-    let matrix = Array.from(Chidos);
-    matrix.push(Expandidos.map(data => {
-        return !ListContainsObject(data, matrix)
-    })
-    )
-    return {
-        "Frontera": Frontera,
-        "Expandidos": Expandidos,
-        "Chidos": Chidos,
-        "Matriz": matrix
-    }
-}
-
-module.exports.verVecinos = verVecinos
-
-const objectsAreSame = (x, y) => {
-    let objectsAreSame = true;
-    for (let propertyName in x) {
-        if (x[propertyName] !== y[propertyName]) {
-            objectsAreSame = false;
-            break;
-        }
-    }
-    return objectsAreSame;
-}
-const ListContainsObject = (a, b) => {
-    let result;
-    b.forEach(val => {
-        result = objectsAreSame(a, val) ? a : ""
-    });
-    return result !== "" ? result : {};
-}
